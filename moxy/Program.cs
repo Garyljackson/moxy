5ab1c642474c56358f74a688469cb6e73d5bd3e8
Gary Jackson <garyljackson@gmail.com>
2022-08-09 21:09:03 +1000

    Add output caching

--- moxy/Program.cs
+++ moxy/Program.cs
@@ -1,34 +1,20 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddOutputCache(options =>
+{
+    options.AddBasePolicy(policyBuilder => policyBuilder.With(c => c.HttpContext.Request.Path.StartsWithSegments("/")).Expire(TimeSpan.FromMinutes(10)));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseOutputCache();
+app.MapReverseProxy();
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
